e1a0d4533b8e36313c9f7fe907203ee973d15616
andy888jam <andy888jam@gmail.com>
2021-12-08 12:50:58 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
+const exphbs = require('express-handlebars')
+    // 建立名為hbs的樣版引擎，並設定express的相關參數
+app.engine('hbs', exphbs({
+        defaultLayout: 'main',
+        //指定副檔名
+        extname: '.hbs'
+    }))
+    //啟用樣版引擎hbs
+app.set('view engine', 'hbs')
 
 mongoose.connect('mongodb://localhost/todo-list')
 
@@ -14,7 +23,7 @@ db.once('open', () => {
 })
 
 app.get('/', (req, res) => {
-    res.send('hello, world!')
+    res.render('index')
 })
 
 app.listen(3000, () => {
