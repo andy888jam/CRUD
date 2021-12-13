57b793c62bd0c9f9d6fa7f2b8f7390f301bede67
andy888jam <andy888jam@gmail.com>
2021-12-13 16:44:07 +0800

    feat: modify index

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 const exphbs = require('express-handlebars')
-    // 建立名為hbs的樣版引擎，並設定express的相關參數
+const Todo = require('./models/todo') //載入todo model
+
+// 建立名為hbs的樣版引擎，並設定express的相關參數
 app.engine('hbs', exphbs({
-        defaultLayout: 'main',
-        //指定副檔名
-        extname: '.hbs'
-    }))
-    //啟用樣版引擎hbs
-app.set('view engine', 'hbs')
+    defaultLayout: 'main',
+    extname: '.hbs' //指定副檔名
+}))
+app.set('view engine', 'hbs') //啟用樣版引擎hbs
 
 mongoose.connect('mongodb://localhost/todo-list')
 
@@ -18,12 +18,17 @@ const db = mongoose.connection
 db.on('error', () => {
     console.log('Mongodb error!')
 })
+
 db.once('open', () => {
     console.log('Mongodb connected!')
 })
 
 app.get('/', (req, res) => {
-    res.render('index')
+    Todo.find() //取出Todo model所有資料
+        .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+        .then(todos => res.render('index', { todos })) //將資料傳給index樣板
+        .catch(error => console.log(error)) //錯誤處理
+
 })
 
 app.listen(3000, () => {
